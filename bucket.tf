87a3753c240b39bf8ade53245e2df3d851d7b961
Liam Deacon <107563512+LiamDeaconAntarcticaAM@users.noreply.github.com>
2022-12-21 11:13:06 +0000

    Refactor to avoid use of deprecated terraform apis

--- bucket.tf
+++ bucket.tf
@@ -2,11 +2,18 @@ resource "aws_s3_bucket" "short_urls_bucket" {
   bucket = var.short_url_domain
   acl    = "public-read"
 
-  website {
-    index_document = "index.html"
-    error_document = "error.html"
+  tags = local.tags
+}
+
+resource "aws_s3_bucket_website_configuration" "short_urls_bucket_website_config" {
+  bucket = aws_s3_bucket.short_urls_bucket.id
+
+    index_document {
+    suffix = "index.html"
   }
-  tags = {
-    Project = "short_urls"
+
+  error_document {
+    key = "error.html"
   }
+
 }
